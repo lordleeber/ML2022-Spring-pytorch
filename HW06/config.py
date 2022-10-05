12753a4010863baf5391e3b5224151a7a9ccc943
lordleeber <lordleeber@gmail.com>
2022-10-05 14:18:46 +0800

    Add pdf files.

--- HW06/config.py
+++ HW06/config.py
@@ -5,7 +5,7 @@ config = {
     "model_type": "GAN",
     "batch_size": 64,
     "lr": 1e-4,
-    "n_epoch": 1,
+    "n_epoch": 100,
     "n_critic": 1,
     "z_dim": 100,
     "workspace_dir": workspace_dir, # define in the environment setting
